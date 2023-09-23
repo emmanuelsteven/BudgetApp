449da45cd4ba1ce9b338cce6edb8e0d878e36c5c
emmanuelsteven <write2stevemmy@gmail.com>
2023-09-23 21:12:30 +0100

    CR changes

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,12 +4,15 @@ class PurchasesController < ApplicationController
     @category = Category.includes(:purchases).find(params[:category_id])
     @purchases = @category.purchases.order(created_at: :desc)
     @total_purchase_amount = @category.purchases.sum(:amount)
-    # @total_amount = @purchases.sum(:amount)
   end
 
   def new
+    @attribute = []
     @category = Category.find(params[:category_id])
     @purchase = Purchase.new
+    @attribute << @purchase
+    @categories = current_user.categories
+    @attribute << @categories
   end
 
   def show
@@ -19,14 +22,16 @@ class PurchasesController < ApplicationController
 
   def create
     @category = Category.find(params[:category_id])
-    @new_purchase = @category.purchases.new(purchase_params)
+    @new_purchase = Purchase.new(purchase_params)
     @new_purchase.author_id = current_user.id
+    @new_purchase.category_id = params[:purchase][:category_id] # Assign the selected category_id from the form
+
     if @new_purchase.save
       flash[:notice] = "#{@new_purchase.name} transaction created"
       redirect_to category_purchases_path(@category)
     else
       flash[:alert] = 'Kindly fill all required fields'
-      puts @new_purchase.errors.full_messages # Add this line to print validation errors to the console
+      puts @new_purchase.errors.full_messages
       render 'new'
     end
   end
@@ -34,6 +39,6 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase).permit(:name, :amount)
+    params.require(:purchase).permit(:name, :amount, :category_id)
   end
 end
